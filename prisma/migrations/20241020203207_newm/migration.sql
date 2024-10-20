14860defa2b9e8094712f19289a6d4848f6bc0bc
Djimra NGARLEITA <dngarleita@gmail.com>
2024-10-20 23:39:36 +0300

    fix(#31): Remove recursive references in .env

--- prisma/migrations/20241020203207_newm/migration.sql
+++ prisma/migrations/20241020203207_newm/migration.sql
@@ -2,10 +2,16 @@
 CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
 
 -- CreateEnum
+CREATE TYPE "UserGender" AS ENUM ('Female', 'Male');
+
+-- CreateEnum
 CREATE TYPE "NoteType" AS ENUM ('NOTE', 'QUOTE');
 
 -- CreateEnum
 CREATE TYPE "AccountStatus" AS ENUM ('CREATED', 'ACTIVATED', 'SUSPENDED');
+
+-- CreateEnum
+CREATE TYPE "LibraryShelf" AS ENUM ('LIKE', 'HAVE', 'WANT_TO_READ', 'READING', 'RED');
 
 -- CreateTable
 CREATE TABLE "user" (
@@ -14,11 +20,12 @@ CREATE TABLE "user" (
     "email" TEXT NOT NULL,
     "phoneNumber" VARCHAR(24) NOT NULL,
     "accountStatus" "AccountStatus" NOT NULL DEFAULT 'CREATED',
+    "avatar" VARCHAR(255),
     "password" VARCHAR(128) NOT NULL,
     "salt" VARCHAR(255) NOT NULL,
     "role" "Role" NOT NULL DEFAULT 'USER',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updateAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
@@ -26,14 +33,13 @@ CREATE TABLE "user" (
 -- CreateTable
 CREATE TABLE "profile" (
     "id" UUID NOT NULL,
-    "gender" VARCHAR(32),
+    "gender" "UserGender",
     "birthday" TIMESTAMP(3),
-    "profilePicture" VARCHAR(128),
-    "coverPicture" VARCHAR(128),
-    "timeZone" VARCHAR(64),
+    "coverPicture" VARCHAR(255),
+    "timeZone" VARCHAR(255),
     "bio" VARCHAR(512),
-    "favoriteAuthor" VARCHAR(64),
-    "favoriteBook" VARCHAR(128),
+    "favoriteAuthor" VARCHAR(128),
+    "favoriteBook" VARCHAR(255),
     "favoriteGenres" TEXT[],
     "centerOfInterest" TEXT[],
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -46,18 +52,18 @@ CREATE TABLE "profile" (
 CREATE TABLE "book" (
     "id" UUID NOT NULL,
     "title" VARCHAR(255) NOT NULL,
-    "subtitle" VARCHAR(255) NOT NULL,
+    "subtitle" VARCHAR(255),
     "isbn" VARCHAR(64),
     "authors" TEXT[],
     "pageCount" INTEGER NOT NULL,
     "summary" TEXT,
-    "illustration" VARCHAR(255),
-    "publicationDate" TIMESTAMP(3),
-    "publisher" VARCHAR(128) NOT NULL,
-    "edition" VARCHAR,
+    "cover" VARCHAR(255),
+    "publicationDate" DATE,
+    "publisher" VARCHAR(128),
+    "edition" VARCHAR(255),
     "editors" TEXT[],
-    "language" VARCHAR NOT NULL,
-    "genre" VARCHAR(64) NOT NULL,
+    "language" VARCHAR(128) NOT NULL,
+    "genre" VARCHAR(64),
     "tags" TEXT[],
     "dimensions" VARCHAR(128),
 
@@ -89,6 +95,7 @@ CREATE TABLE "library" (
     "rating" INTEGER,
     "userId" UUID NOT NULL,
     "bookId" UUID NOT NULL,
+    "shelf" "LibraryShelf" DEFAULT 'HAVE',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -98,8 +105,8 @@ CREATE TABLE "library" (
 -- CreateTable
 CREATE TABLE "post" (
     "id" UUID NOT NULL,
-    "text" TEXT NOT NULL,
-    "reference" VARCHAR(128) NOT NULL,
+    "text" VARCHAR(1000) NOT NULL,
+    "reference" VARCHAR(128),
     "medias" TEXT[],
     "userId" UUID NOT NULL,
     "bookId" UUID,
@@ -155,22 +162,22 @@ CREATE UNIQUE INDEX "comment_parentId_key" ON "comment"("parentId");
 CREATE INDEX "comment_parentId_idx" ON "comment"("parentId");
 
 -- AddForeignKey
-ALTER TABLE "profile" ADD CONSTRAINT "profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "profile" ADD CONSTRAINT "profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "note" ADD CONSTRAINT "note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "note" ADD CONSTRAINT "note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "note" ADD CONSTRAINT "note_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "book"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "library" ADD CONSTRAINT "library_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "library" ADD CONSTRAINT "library_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "library" ADD CONSTRAINT "library_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "post" ADD CONSTRAINT "post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "post" ADD CONSTRAINT "post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "post" ADD CONSTRAINT "post_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "book"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -179,19 +186,19 @@ ALTER TABLE "post" ADD CONSTRAINT "post_bookId_fkey" FOREIGN KEY ("bookId") REFE
 ALTER TABLE "comment" ADD CONSTRAINT "comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "comment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "comment" ADD CONSTRAINT "comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "comment" ADD CONSTRAINT "comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "comment" ADD CONSTRAINT "comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "comment" ADD CONSTRAINT "comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "postlike" ADD CONSTRAINT "postlike_postId_fkey" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "postlike" ADD CONSTRAINT "postlike_postId_fkey" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "postlike" ADD CONSTRAINT "postlike_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "postlike" ADD CONSTRAINT "postlike_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "commentlike" ADD CONSTRAINT "commentlike_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "comment"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "commentlike" ADD CONSTRAINT "commentlike_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "commentlike" ADD CONSTRAINT "commentlike_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "commentlike" ADD CONSTRAINT "commentlike_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
